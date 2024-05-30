d55e3d4095ff2c39659afd109f804a9af058aa01
Adam Amster <adam.amster@alleninstitute.org>
2024-05-30 10:24:53 -0400

    loss function flattening

--- src/seq2seq_translation/train_evaluate.py
+++ src/seq2seq_translation/train_evaluate.py
@@ -51,13 +51,9 @@ def train_epoch(
                 target_tensor=target_input_tensor
             )
 
-        batch_size = target_tensor.shape[0]
-        C = decoder_outputs.shape[-1]
-        T = target_tensor.shape[-1]
-
         loss = criterion(
-            decoder_outputs[:, :T].reshape(batch_size * T, C),
-            target_tensor.view(batch_size * T)
+            decoder_outputs.view(-1, decoder_outputs.size(-1)),
+            target_tensor.view(-1)
         )
         loss.backward()
 
